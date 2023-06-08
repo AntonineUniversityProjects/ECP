3ca309162412c96041bad961d38bf8db67626434
Roygebrayel <roy.gebrayel11@gmail.com>
2023-06-08 22:29:01 +0300

    testing functional and classes components

---
+++ src/components/Hello.js
@@ -0,0 +1,8 @@
+// function Hello (){
+//     return <h1> hello how are you </h1>
+// }
+
+const Hello = () => <h1> hello hbb</h1>;
+
+
+export default Hello;
---
+++ src/components/Message.js
@@ -0,0 +1,12 @@
+
+import { Component } from "react";
+
+class Message extends Component {
+    render(){
+        return <p> this is a class hbb</p>
+    } 
+    
+
+}
+
+export default Message;
